bbb8937e40b64ed50a8804329b5bd79e74b629a9
Tolnai <tolnai.milan22@gmail.com>
2025-05-15 16:23:27 +0200

    testing ci/cd

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { Game } from "./core/Game";
 import { safeInput, validatePlayerCount } from "./utilities/inputValidation";
 
 
-const playerCount = validatePlayerCount("Enter number of players (min 3 or type 'exit'): ");
+const playerCount = validatePlayerCount("Enter number of players (min 3 or type 'exit '): ");
 
 
 const game = new Game(playerCount);
